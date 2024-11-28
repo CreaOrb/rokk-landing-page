21a186465e3b03f61b7702cff1a84962d15184ce
kaif <syedkaift@gmail.com>
2024-11-29 00:50:02 +0530

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import Banner from "../src/assets/img/bg-banner.png";
 const App = () => {
     return (
 
-        <main className="relative scroll-smooth">
-            <div className="img absolute w-screen max-md:top-16 top-12 h-screen -z-50 ">
+        <main className="relative scroll-smooth overflow-x-hidden">
+            <div className="overflow-x-hidden img absolute w-screen max-md:top-16 top-12 h-screen -z-50 ">
                 <motion.img
                     initial={{opacity:0, scale:0.9}}
                     animate={{opacity:1,scale:1 }}
