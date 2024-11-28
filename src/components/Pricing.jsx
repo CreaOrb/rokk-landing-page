b497e12095892a9139d26c9cd98fa07a5ee3a9f2
kaif <syedkaift@gmail.com>
2024-11-28 23:43:44 +0530

    v2

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -32,16 +32,16 @@ const Pricing = () => {
         },
 
     ];
-    const [emp, setEmp] = useState({price:"€10.99"});
+    const [emp] = useState({price:"€10.99"});
     const [month, setMonth] = useState({price:"€10.99"});
     function changePriceMonth() {
         setMonth({price:emp.price});
     }
-    const [yearly, setYearly] = useState({price:"€131.88"});
+    const [yearly] = useState({price:"€131.88"});
     function changePriceYear() {
         setMonth({price:yearly.price});
     }
-    const [student, setStudent] = useState({price:"€65.94"});
+    const [student] = useState({price:"€65.94"});
     function changePriceStudent() {
         setMonth({price:student.price});
     }
@@ -54,14 +54,15 @@ const Pricing = () => {
 
         <div className="pricing-section bg-black-400 bg-gradient-to-t from-black-300 from-0% to-black-400 to-25% text-Orange-200">
             <div className="container mx-auto max-md:px-6">
-                <motion.div animate={{y:0}}
-                            initial={{y:200,opacity:0}}
-                            whileInView={{opacity:1, y:0}}
-                            transition={{duration:2}}
-
-                            viewport={{ once: true, amount: 0.2 }}
+                <motion.div
                             className="pricing-plans max-w-4xl pb-32 max-md:pb-24 mx-auto">
-                    <div className="content-head text-center">
+                    <motion.div
+                        initial={{y:100, opacity:0,}}
+                        animate={{ y:-100, }}
+                        whileInView={{opacity:1, y:0}}
+                        viewport={{ once: true}}
+                        transition={{ duration: 0.5, delay: 0.2 }}
+                        className="content-head text-center">
                         <h1 className="text-center text-6xl max-w-3xl max-md:text-4xl mx-auto font-TitlingGothicFB text-white tracking-wide leading-none uppercase">
                             We have got a <span
                                 className="bg-gradient-to-r from-Orange-200 to-Orange-100 text-transparent bg-clip-text">plans & Pricing</span> that’s
@@ -73,23 +74,28 @@ const Pricing = () => {
 
 
 
-                        <div className="button-group mt-12 bg-black-200 w-fit mx-auto p-1 max-md:rounded-3xl rounded-full text-center">
-                            <button onClick={changePriceMonth}
-                                className={`min-w-44 max-md:min-w-full text-base font-Poppins-Regular text-black-50  py-2 text-center focus:text-white focus:bg-black-250 rounded-full`
+                        {/*<div className="button-group mt-12 bg-black-200 w-fit mx-auto p-1 max-md:rounded-3xl rounded-full text-center">*/}
+                        {/*    <button onClick={changePriceMonth}*/}
+                        {/*        className={`min-w-44 max-md:min-w-full text-base font-Poppins-Regular text-black-50  py-2 text-center focus:text-white focus:bg-black-250 rounded-full`*/}
 
-                            }>Monthly
-                            </button>
-                            <button onClick={changePriceYear}
-                                className=" min-w-44 max-md:min-w-full text-base font-Poppins-Regular text-black-50  focus:bg-black-250 focus:text-white   py-2  rounded-full">
-                                Annual (-16%)
-                            </button>
-                            <button onClick={changePriceStudent}
-                                className="min-w-44 max-md:min-w-full text-base font-Poppins-Regular text-black-50 focus:bg-black-250  focus:text-white  py-2  rounded-full">
-                                Students (-50%)
-                            </button>
-                        </div>
-                    </div>
-                    <div className="content-body mx-auto mt-20 max-md:mt-12">
+                        {/*    }>Monthly*/}
+                        {/*    </button>*/}
+                        {/*    <button onClick={changePriceYear}*/}
+                        {/*        className=" min-w-44 max-md:min-w-full text-base font-Poppins-Regular text-black-50  focus:bg-black-250 focus:text-white   py-2  rounded-full">*/}
+                        {/*        Annual (-16%)*/}
+                        {/*    </button>*/}
+                        {/*    <button onClick={changePriceStudent}*/}
+                        {/*        className="min-w-44 max-md:min-w-full text-base font-Poppins-Regular text-black-50 focus:bg-black-250  focus:text-white  py-2  rounded-full">*/}
+                        {/*        Students (-50%)*/}
+                        {/*    </button>*/}
+                        {/*</div>*/}
+                    </motion.div>
+                    <motion.div
+                        initial={{y:100, opacity:0,}}
+                        animate={{ y:0, }}
+                        whileInView={{opacity:1, y:-50}}
+                        transition={{ duration: 0.5, delay: 0.4 }}
+                        className="content-body mx-auto mt-28 max-md:mt-12">
                         <div
                             className="card bg-black-100 p-[2px] rounded-3xl  hover:bg-gradient-to-l  bg-gradient-to-r from-Orange-200 to-black-100/10 transition ease-in duration-300">
                             <div className="body p-[1px] bg-black-100 rounded-3xl">
@@ -133,7 +139,7 @@ const Pricing = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </motion.div>
                 </motion.div>
             </div>
         </div>
